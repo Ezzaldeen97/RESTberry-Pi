d84acdf854072a235216f40a8a03d5590af52760
Ezzaldeen <e.alribi@enginius.de>
2024-08-04 17:47:56 +0200

    enhanced the mysql schema, added primary keys and changed the length of some columns

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -1,20 +1,24 @@
 CREATE DATABASE IF NOT EXISTS QAIA_DB;
 CREATE TABLE IF NOT EXISTS Arrivals(
-airline varchar(25),
-origin varchar(20),
-Flight_number varchar(20),
-scheduled_time datetime,
-estimated_time datetime,
+airline varchar(25) NOT NULL,
+origin varchar(20) NOT NULL,
+Flight_number varchar(10) NOT NULL,
+scheduled_time datetime NOT NULL,
+estimated_time datetime NOT NULL,
 gate INT,
-flight_status varchar(10)
+flight_status varchar(20),
+PRIMARY KEY (scheduled_time, Flight_number)
+
 );
 
 CREATE TABLE IF NOT EXISTS Departures(
-airline varchar(25),
-destination varchar(20),
-Flight_number varchar(20),
-scheduled_time datetime,
-estimated_time datetime,
+airline varchar(25) NOT NULL,
+destination varchar(20) NOT NULL,
+Flight_number varchar(10) NOT NULL,
+scheduled_time datetime NOT NULL,
+estimated_time datetime NOT NULL,
 gate INT,
-flight_status varchar(10)
+flight_status varchar(20),
+PRIMARY KEY (scheduled_time, Flight_number)
+
 );
