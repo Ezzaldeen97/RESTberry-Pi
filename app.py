d8f540b708da3f54a8827281bea311e80492ef1c
Ezzaldeen <e.alribi@enginius.de>
2024-09-01 18:29:18 +0200

    Added the flask app to the stack, and renamed the file to be app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask,json
+from flask import Flask,render_template
 import backend.database as db
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='frontend/templates')
 
 @app.route("/api/flights/arrivals", methods =["GET"])
 def get_arrivals():
     select_statement = "SELECT * FROM Arrivals"
     cursor.execute(select_statement)
     flights = cursor.fetchall()
-    return flights
+    return render_template("table.html",flights=flights)
 
 @app.route("/api/flights/departures", methods =["GET"])
 def get_departures():
@@ -29,13 +29,13 @@ def get_departures_flight(flight_number):
 
     cursor.execute(select_statement,(flight_number,))
     flights = cursor.fetchall()
-    return flights
+    return render_template("table.html",flights=flights)
 
 if __name__ == '__main__':
     db.connect()
     con = db.get_connection()
     cursor = con.get_cursor()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
         
 
