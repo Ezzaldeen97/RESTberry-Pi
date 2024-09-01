7fef646ac68cc017cb0aa1b3829dfe275a4d1814
Ezzaldeen <e.alribi@enginius.de>
2024-09-01 18:44:17 +0200

    configured the html file to take the base url and append to it, and the app.py should render the db query to the html file

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ def get_departures():
     select_statement = "SELECT * FROM Departures"
     cursor.execute(select_statement)
     flights = cursor.fetchall()
-    return flights            
+    return render_template("table.html",flights=flights)
 
 @app.route('/api/flights/departures/<flight_number>', methods=['GET'])
 def get_arrivals_flight(flight_number):
     select_statement = "SELECT * FROM Departures WHERE flight_number = %s"
     cursor.execute(select_statement,(flight_number,))
     flights = cursor.fetchall()
-    return flights
+    return render_template("table.html",flights=flights)
 @app.route('/api/flights/arrivals/<flight_number>', methods=['GET'])
 def get_departures_flight(flight_number):
     select_statement = "SELECT * FROM Arrivals WHERE flight_number = %s"
